364505f9735f7fb63f7dfcb0134987f10078afed
LeNiglo <lefrantguillaume@gmail.com>
2020-06-18 01:05:50 +0200

    Moved <head> to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Head>
+      <title>Guillaume Lefrant</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </>)
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
-import Head from 'next/head'
-
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Guillaume Lefrant</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <h1 className="title">
           Guillaume Lefrant
