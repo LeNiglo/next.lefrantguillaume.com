60597e3f84d2a4dafb4fd7b9876ee48456459095
LeNiglo <lefrantguillaume@gmail.com>
2020-06-20 22:43:43 +0200

    No need to Typescript

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
       <title>Guillaume Lefrant</title>
