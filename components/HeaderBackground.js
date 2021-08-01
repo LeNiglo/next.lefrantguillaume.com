69f4505d2978a3f9f9f0f1cebb759f17d5901e6f
LeNiglo <lefrantguillaume@gmail.com>
2021-08-01 17:49:34 +0200

    Restart using tailwind

---
+++ components/HeaderBackground.js
@@ -0,0 +1,8 @@
+export default function HeaderBackground () {
+    return (
+        <div className="h-screen w-screen bg-grey-dark text-grey-light flex flex-col items-center justify-center animate-header-background">
+            <h1 className="text-6xl font-light">Guillaume Lefrant</h1>
+            <p className="text-3xl">Développeur Web FullStack</p>
+        </div>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
-    extend: {},
+    colors: {
+      grey: {
+        light: '#8AB5D4',
+        DEFAULT: '#7194AD',
+        dark: '#2E3D47',
+      }
+    },
+    extend: {
+      animation: {
+        'header-background': 'header-background 5s cubic-bezier(.8, 0, .2, 1) 2s infinite alternate both',
+      },
+      keyframes: {
+        'header-background': {
+          '0%': {
+            backgroundColor: "theme('colors.grey.dark')",
+            color: "theme('colors.grey.light')",
+          },
+          '100%': {
+            backgroundColor: "theme('colors.grey.light')",
+            color: "theme('colors.grey.dark')",
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
